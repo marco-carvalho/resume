a933defa5126830643e360955f6ee74c60cc5aeb
Marco Carvalho <marcolucio27@gmail.com>
2018-02-06 11:43:24 -0200

    delete useless files

--- src/resume/controller.js
+++
@@ -1,6 +0,0 @@
-export default [
-    '$scope',
-    function ($scope) {
-        $scope.resume = 'Um cientista da computação em formação, apaixonado por desenvolvimento web e sobre os bons usos de novas tecnologias, e focado em melhorar e aplicar os meus conhecimentos e habilidades - tanto os conquistados academicamente quanto os de forma independente - em experiências que me façam crescer profissionalmente.';
-    }
-];
