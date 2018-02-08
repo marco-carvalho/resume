a46ea446f4ddd4bf35c9b3bf346c31937cf42117
Marco Carvalho <marcolucio27@gmail.com>
2018-02-08 13:42:00 -0200

    update main js

--- src/main.js
+++ src/main.js
@@ -9,10 +9,7 @@ import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
