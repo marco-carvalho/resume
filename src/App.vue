2d1b13ffb8c387b1b419ddfeec7dff015f780614
Marco Carvalho <marcolucio27@gmail.com>
2018-02-08 23:40:46 -0200

    add home view + move components to it

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,3 @@
 <template lang="pug">
-  #app.mx-auto.col-7
-    resume-header
-    resume-resume
-    resume-experiences
-    .row
-      .col-6
-        resume-education
-      .col-6
-        resume-languages
-    resume-skills
-    resume-footer
+  router-view
 </template>
-
-<script>
-  import Education from "./views/Education"
-  import Experiences from "./views/Experiences"
-  import Footer from "./views/Footer"
-  import Header from "./views/Header"
-  import Languages from "./views/Languages"
-  import Resume from "./views/Resume"
-  import Skills from "./views/Skills"
-
-  export default {
-    components: {
-      'resume-education': Education,
-      'resume-experiences': Experiences,
-      'resume-footer': Footer,
-      'resume-header': Header,
-      'resume-languages': Languages,
-      'resume-resume': Resume,
-      'resume-skills': Skills
-    }
-  }
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+  #app.mx-auto.col-9
+    resume-header
+    resume-resume
+    resume-experiences
+    .row
+      .col-6
+        resume-education
+      .col-6
+        resume-languages
+    resume-skills
+    hr
+    resume-footer
+</template>
+
+<script>
+  import Education from "@/components/Education"
+  import Experiences from "@/components/Experiences"
+  import Footer from "@/components/Footer"
+  import Header from "@/components/Header"
+  import Languages from "@/components/Languages"
+  import Resume from "@/components/Resume"
+  import Skills from "@/components/Skills"
+
+  export default {
+    components: {
+      'resume-education': Education,
+      'resume-experiences': Experiences,
+      'resume-footer': Footer,
+      'resume-header': Header,
+      'resume-languages': Languages,
+      'resume-resume': Resume,
+      'resume-skills': Skills
+    }
+  }
+</script>
