29df83aacbb9b173de7350ad133f792c322e0847
Marco Carvalho <marcolucio27@gmail.com>
2018-02-15 19:58:44 -0200

    move home to app

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,36 @@
 <template lang="pug">
-  router-view
+  #app.mx-auto.col-11
+    Header
+    Resume
+    Experiences
+    .row
+      .col-6
+        Education
+      .col-6
+        Languages
+    Skills
+    hr
+    Footer
 </template>
+
+<script>
+  import Education from "@/components/Education"
+  import Experiences from "@/components/Experiences"
+  import Footer from "@/components/Footer"
+  import Header from "@/components/Header"
+  import Languages from "@/components/Languages"
+  import Resume from "@/components/Resume"
+  import Skills from "@/components/Skills"
+
+  export default {
+    components: {
+      Education,
+      Experiences,
+      Footer,
+      Header,
+      Languages,
+      Resume,
+      Skills
+    }
+  }
+</script>
