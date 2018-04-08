2e7f02948bed27937cd63f75ebe9cac20883bcc4
Marco Carvalho <marcolucio27@gmail.com>
2018-04-08 15:12:21 -0300

    eslint --fix

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome'
-import '@fortawesome/fontawesome-free-solid/'
-import '@fortawesome/fontawesome-free-brands/'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome";
+import "@fortawesome/fontawesome-free-solid/";
+import "@fortawesome/fontawesome-free-brands/";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
