e8df14810faf62c632790903467c226d4529137d
Marco Carvalho <marcolucio27@gmail.com>
2018-05-12 15:53:57 -0300

    add link to project

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
-  .text-center
-    span.mr-1 Feito com
-    a(href='https://vuejs.org/') VueJS
-    span.mr-1 ,
-    a(href='https://pugjs.org') Pug
-    span.mr-1 ,
-    a(href='https://webpack.js.org') Webpack
-    span.mx-1 e
-    a(href='https://getbootstrap.com/') Bootstrap
-    span .
+  div
+    .text-center
+      span.mr-1 Feito com
+      a(href='https://vuejs.org/') VueJS
+      span.mr-1 ,
+      a(href='https://pugjs.org') Pug
+      span.mr-1 ,
+      a(href='https://webpack.js.org') Webpack
+      span.mx-1 e
+      a(href='https://getbootstrap.com/') Bootstrap
+      span .
+    .text-center
+      a(href="/") http://marco-carvalho.github.io/resume
 </template>
