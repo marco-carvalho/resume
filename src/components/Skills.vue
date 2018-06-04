1b80a3cf26d929d2d646e13450314be6bc51decb
Marco Carvalho <marcolucio27@gmail.com>
2018-06-03 21:01:16 -0300

    update layout

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,14 +2,11 @@
   div
     h2.font-weight-bold.text-uppercase.text-center.m-0 {{name}}
     hr.mt-0
-    .row.text-center
-      .col-md-6.mb-3(v-for="skill in skills")
-        h5.font-weight-bold.text-uppercase.m-0 {{skill.name}}
-        hr.mt-0
-        .list-inline-item(v-for="tecnology in skill.tecnologies")
-          .list-inline-item.bg-dark.p-1.rounded.text-white.mt-1 {{tecnology.name}}
-          .list-inline-item(v-for="subtecnology in tecnology.subtecnologies")
-            .list-inline-item.bg-secondary.p-1.rounded.text-white.mt-1 {{subtecnology}}
+    .text-center.mb-3(v-for="skill in skills")
+      .list-inline-item(v-for="tecnology in skill.tecnologies")
+        .list-inline-item.bg-dark.p-1.rounded.text-white.mt-1 {{tecnology.name}}
+        .list-inline-item(v-for="subtecnology in tecnology.subtecnologies")
+          .list-inline-item.bg-secondary.p-1.rounded.text-white.mt-1 {{subtecnology}}
 </template>
 
 <script>
