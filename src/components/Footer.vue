7009a1f8eb43c01c6180783562ab638cdedf11d6
Marco Carvalho <marcolucio27@gmail.com>
2018-06-03 21:00:10 -0300

    remove webpack

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,8 +5,6 @@
       a(href='https://vuejs.org/') VueJS
       span.mr-1 ,
       a(href='https://pugjs.org') Pug
-      span.mr-1 ,
-      a(href='https://webpack.js.org') Webpack
       span.mx-1 e
       a(href='https://getbootstrap.com/') Bootstrap
       span .
