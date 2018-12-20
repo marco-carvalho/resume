72db2876834b2e2a63d05827e487b8378417cd48
Marco Carvalho <marcolucio27@gmail.com>
2018-12-20 00:13:54 -0200

    finally finished that shit

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -10,7 +10,7 @@ export default {
   data () {
     return {
       name: "Resumo",
-      resume: 'Um cientista da computação em formação, apaixonado por desenvolvimento web e sobre os bons usos de novas tecnologias, e focado em melhorar e aplicar os meus conhecimentos e habilidades - tanto os conquistados academicamente quanto os de forma independente - em experiências que me façam crescer profissionalmente.'
+      resume: 'Um cientista da computação apaixonado por desenvolvimento web e sobre os bons usos de novas tecnologias, e focado em melhorar e aplicar os meus conhecimentos e habilidades - tanto os conquistados academicamente quanto os de forma independente - em experiências que me façam crescer profissionalmente.'
     }
   }
 }
