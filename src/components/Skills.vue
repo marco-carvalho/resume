2e66c00ba7dc530fad7d31f4487a397572944915
Marco Carvalho <marcolucio27@gmail.com>
2019-01-20 13:31:07 -0200

    update skills layout

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div
+.mb-3
   .bg-dark.text-uppercase.text-center.text-white.rounded
     h2.font-weight-bold.p-1 {{name}}
   .row.mb-2(v-for="skill in skills")
@@ -8,9 +8,10 @@ div
         h2.font-weight-bold.p-1 {{skill.name}}
     .col
       .row.mb-1(v-for="tecnology in skill.tecnologies")
-        .list-inline-item.bg-dark.p-1.rounded.text-white {{tecnology.name}}
+        .list-inline-item
+          .bg-dark.p-1.rounded.text-white {{tecnology.name}}
         .list-inline-item(v-for="subtecnology in tecnology.subtecnologies")
-          .list-inline-item.bg-secondary.p-1.rounded.text-white {{subtecnology}}
+          .bg-secondary.p-1.rounded.text-white {{subtecnology}}
 </template>
 
 <script>
