8d877c75d1a5365c8d3a396f3ae310cd3a5a33ed
Marco Carvalho <marcolucio27@gmail.com>
2020-06-08 12:54:30 -0300

    fix "quotes" and "multiline-comment-style"

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import "@/css/index.css";
-import {library, dom} from "@fortawesome/fontawesome-svg-core";
-import {fas} from "@fortawesome/free-solid-svg-icons";
-import {fab} from "@fortawesome/free-brands-svg-icons";
-import moment from 'moment';
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import moment from "moment";
 
 library.add(fas, fab);
 dom.watch();
 
 Vue.config.productionTip = false;
 Vue.prototype.$moment = moment;
-Vue.prototype.$moment.locale('pt-br');
+Vue.prototype.$moment.locale("pt-br");
 
 new Vue({
   router,
